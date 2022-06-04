492687be08cc5e252345ba4e0a7385f8172e4325
Thiago Melo <thiagomelo@super.ufam.edu.br>
2022-06-04 11:48:30 -0400

    conexao para o banco

--- diarias/main.py
+++ diarias/main.py
@@ -9,5 +9,11 @@ import json
 #lendo as configurações de conexão do arquivo json
 with open('config.json') as config_file:
     dados_conexao = json.load(config_file)
-    
-#sakdokoadokaodkkapd
+
+#conexão ao banco salva na variavel dbsasi
+dbsasi = mysql.connector.connect(
+   **dados_conexao
+)
+
+#cursor apontando para o banco
+sasi_cursor = dbsasi.cursor()
