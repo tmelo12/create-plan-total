026bb65fd65b18f051651f685c66a1d3ead7fad9
Thiago Braga de Melo <103153661+tmelo12@users.noreply.github.com>
2022-06-07 16:14:54 -0400

    Add files via upload

--- geralzona/main.py
+++ geralzona/main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from calha import nomeCalha
 
 #lendo as configurações de conexão do arquivo json
-with open('/home/thiago/Documentos/notebooks/create-plan-total/geralzona/config.json') as config_file:
+with open('/home/dev03/Downloads/create-plan-total-master/geralzona/config.json') as config_file:
     dados_conexao = json.load(config_file)
     
 #conexão ao banco salva na variavel dbsasi
@@ -26,6 +26,7 @@ sasi_cursor.execute(query)
 COLUNAS = [
     'id_sasi',
     'municipio',
+    'cep',
     'card',
     'status_valecard',
     'cpf',
@@ -61,27 +62,83 @@ COLUNAS = [
 ]
 
 df_dados_da_base = pd.DataFrame(columns=COLUNAS)
-
+#tipage dos dados
+df_dados_da_base['municipio'] = df_dados_da_base['municipio'].astype(str)
+df_dados_da_base['cpf'] = df_dados_da_base['cpf'].astype(str)
+df_dados_da_base['rg'] = df_dados_da_base['rg'].astype(str)
+df_dados_da_base['nome'] = df_dados_da_base['nome'].astype(str)
+df_dados_da_base['data_de_nascimento'] = df_dados_da_base['data_de_nascimento'].astype(str)
+df_dados_da_base['cd_cartao'] = df_dados_da_base['cd_cartao'].astype(str)
+df_dados_da_base['renda_mensal'] = df_dados_da_base['renda_mensal'].astype(str)
+df_dados_da_base['qual_situacao'] = df_dados_da_base['qual_situacao'].astype(str)
+df_dados_da_base['numero'] = df_dados_da_base['numero'].astype(str)
+df_dados_da_base['tipo_endereco'] = df_dados_da_base['tipo_endereco'].astype(str)
+df_dados_da_base['servido_publico'] = df_dados_da_base['servido_publico'].astype(str)
+df_dados_da_base['trabalhador_informal'] = df_dados_da_base['trabalhador_informal'].astype(str)
+df_dados_da_base['classificacao_moradia'] = df_dados_da_base['classificacao_moradia'].astype(str)
+df_dados_da_base['confirmar_anexo_cartao'] = df_dados_da_base['confirmar_anexo_cartao'].astype(str)
+df_dados_da_base['foto_rg0'] = df_dados_da_base['foto_rg0'].astype(str)
+df_dados_da_base['foto_rg1'] = df_dados_da_base['foto_rg1'].astype(str)
+df_dados_da_base['foto_rg2'] = df_dados_da_base['foto_rg2'].astype(str)
+df_dados_da_base['foto_rg3'] = df_dados_da_base['foto_rg3'].astype(str)
+df_dados_da_base['foto_cd_cartao0'] = df_dados_da_base['foto_cd_cartao0'].astype(str)
+df_dados_da_base['foto_cd_cartao1'] = df_dados_da_base['foto_cd_cartao1'].astype(str)
+df_dados_da_base['foto_cd_cartao2'] = df_dados_da_base['foto_cd_cartao2'].astype(str)
+df_dados_da_base['foto_cd_cartao3'] = df_dados_da_base['foto_cd_cartao3'].astype(str)
+df_dados_da_base['rg_cpf_anexo'] = df_dados_da_base['rg_cpf_anexo'].astype(str)
+df_dados_da_base['rg_cpf_anexo1'] = df_dados_da_base['rg_cpf_anexo1'].astype(str)
+df_dados_da_base['rg_cpf_anexo2'] = df_dados_da_base['rg_cpf_anexo2'].astype(str)
+i=1
 #inserindo linhas vindas da base no dataframe
 for (linha) in sasi_cursor:
     #criando a coluna com nome da calha
     lista=list(linha)
-    lista.append(nomeCalha(linha[1]))
+    lista.append(nomeCalha(str(linha[1])))
     linha=tuple(lista)
     
     #crio um dataframe de uma linha só, pois o mysql retorna uma tupla e pra concatenar preciso de 2 df
     novaLinha = pd.DataFrame([linha], columns = COLUNAS)
-    
     #concatena
     df_dados_da_base = pd.concat ([df_dados_da_base , novaLinha] )
+    print('Linha ',i)
+    i=i+1
 
 #limpando os dados
-df_dados_da_base['cpf'] = df_dados_da_base['cpf'].str.replace('-','')
-df_dados_da_base['cpf'] = df_dados_da_base['cpf'].str.replace('.','')
-df_dados_da_base['cpf'] = df_dados_da_base['cpf'].str.replace(' ','')
+df_dados_da_base['cpf'] = df_dados_da_base['cpf'].str.replace('-','', regex=True).astype(str)
+df_dados_da_base['cpf'] = df_dados_da_base['cpf'].str.replace('.','', regex=True).astype(str)
+df_dados_da_base['cpf'] = df_dados_da_base['cpf'].str.replace(' ','', regex=True).astype(str)
+
+#tipage dos dados
+df_dados_da_base['municipio'] = df_dados_da_base['municipio'].astype(str)
+df_dados_da_base['cpf'] = df_dados_da_base['cpf'].astype(str)
+df_dados_da_base['rg'] = df_dados_da_base['rg'].astype(str)
+df_dados_da_base['nome'] = df_dados_da_base['nome'].astype(str)
+df_dados_da_base['data_de_nascimento'] = df_dados_da_base['data_de_nascimento'].astype(str)
+df_dados_da_base['cd_cartao'] = df_dados_da_base['cd_cartao'].astype(str)
+df_dados_da_base['renda_mensal'] = df_dados_da_base['renda_mensal'].astype(str)
+df_dados_da_base['qual_situacao'] = df_dados_da_base['qual_situacao'].astype(str)
+df_dados_da_base['numero'] = df_dados_da_base['numero'].astype(str)
+df_dados_da_base['tipo_endereco'] = df_dados_da_base['tipo_endereco'].astype(str)
+df_dados_da_base['servido_publico'] = df_dados_da_base['servido_publico'].astype(str)
+df_dados_da_base['trabalhador_informal'] = df_dados_da_base['trabalhador_informal'].astype(str)
+df_dados_da_base['classificacao_moradia'] = df_dados_da_base['classificacao_moradia'].astype(str)
+df_dados_da_base['confirmar_anexo_cartao'] = df_dados_da_base['confirmar_anexo_cartao'].astype(str)
+df_dados_da_base['foto_rg0'] = df_dados_da_base['foto_rg0'].astype(str)
+df_dados_da_base['foto_rg1'] = df_dados_da_base['foto_rg1'].astype(str)
+df_dados_da_base['foto_rg2'] = df_dados_da_base['foto_rg2'].astype(str)
+df_dados_da_base['foto_rg3'] = df_dados_da_base['foto_rg3'].astype(str)
+df_dados_da_base['foto_cd_cartao0'] = df_dados_da_base['foto_cd_cartao0'].astype(str)
+df_dados_da_base['foto_cd_cartao1'] = df_dados_da_base['foto_cd_cartao1'].astype(str)
+df_dados_da_base['foto_cd_cartao2'] = df_dados_da_base['foto_cd_cartao2'].astype(str)
+df_dados_da_base['foto_cd_cartao3'] = df_dados_da_base['foto_cd_cartao3'].astype(str)
+df_dados_da_base['rg_cpf_anexo'] = df_dados_da_base['rg_cpf_anexo'].astype(str)
+df_dados_da_base['rg_cpf_anexo1'] = df_dados_da_base['rg_cpf_anexo1'].astype(str)
+df_dados_da_base['rg_cpf_anexo2'] = df_dados_da_base['rg_cpf_anexo2'].astype(str)
+df_dados_da_base['qual_situacao'] = df_dados_da_base['qual_situacao'].astype(str)
+df_dados_da_base['qual_situacao'] = df_dados_da_base['qual_situacao'].astype(str)
 
 #salvar em excel ordenando por id
-df_dados_da_base.sort_values(by=['id_sasi']).to_excel('/home/thiago/Documentos/notebooks/planilha_geral/dados_cartao_total.xlsx',index=False)
+df_dados_da_base.sort_values(by=['id_sasi']).to_excel('/home/dev03/Downloads/planilhas_op_enchente_2022/planilha_geral/dados_cartao_total.xlsx',index=False)
 
 #fechando a conexão
 sasi_cursor.close()
