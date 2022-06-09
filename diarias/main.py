3e2561ba93808b275dfe19035b305a45affe6e61
Thiago Braga de Melo <103153661+tmelo12@users.noreply.github.com>
2022-06-08 20:40:21 -0400

    correcao dos tipos bytes no script das diarias

--- diarias/main.py
+++ diarias/main.py
@@ -5,7 +5,7 @@ from datetime import date, timedelta
 import json
 from calha import nomeCalha
 #lendo as configurações de conexão do arquivo json
-with open('/home/thiago/Documentos/notebooks/create-plan-total/diarias/config.json') as config_file:
+with open('./config.json') as config_file:
     dados_conexao = json.load(config_file)
 
 #conexão ao banco salva na variavel dbsasi
@@ -17,7 +17,7 @@ dbsasi = mysql.connector.connect(
 sasi_cursor = dbsasi.cursor()
 
 #query com a busca que queremos fazer
-query = ("SELECT * FROM custom_110760000100.vw_cartao_associado WHERE generetedAt LIKE CONCAT (%s,'%');")
+query = ("SELECT * FROM custom_110760000100.cartao_associado WHERE generatedAt LIKE CONCAT (%s,'%');")
 
 #criando as datas para a busca
 data_de_operacao = date(2022, 5, 13) #inicio da operacao
@@ -26,18 +26,21 @@ hoje = date.today() #data atual
 
 #loop para buscar os dados do banco de acordo com as datas
 while(data_de_operacao <= hoje):
-    sasi_cursor.execute(query, (data_de_operacao,))
+    sasi_cursor.execute(query, (data_de_operacao.strftime("%Y-%m-%d"),))
     
     #processo de criar o dataframe
     COLUNAS = [
         'id_sasi',
         'municipio',
+        'cep',
         'card',
         'status_valecard',
         'cpf',
         'rg',
         'nome',
         'data_de_nascimento',
+        'nome_da_mae',
+        'telefone_para_contato',
         'cd_cartao',
         'renda_mensal',
         'qual_situacao',
@@ -66,24 +69,32 @@ while(data_de_operacao <= hoje):
         'calha'
     ]
     df_dados_da_base = pd.DataFrame(columns=COLUNAS)
-    
+    i=1
     for (linha) in sasi_cursor:
         #criando a coluna com nome da calha
         lista=list(linha)
         lista.append(nomeCalha(linha[1]))
+        #decode pois o campo data nascimento esta como longtext no banco
+        lista[8]=lista[8].decode("utf8")
+        
         linha=tuple(lista)
         
         novaLinha = pd.DataFrame([linha], columns = COLUNAS)
         df_dados_da_base = pd.concat ([df_dados_da_base , novaLinha] )
+        print('Linha ',i)
+        i=i+1
     
     #limpando os dados
-    df_dados_da_base['cpf'] = df_dados_da_base['cpf'].str.replace('-','')
-    df_dados_da_base['cpf'] = df_dados_da_base['cpf'].str.replace('.','')
-    df_dados_da_base['cpf'] = df_dados_da_base['cpf'].str.replace(' ','')
+    df_dados_da_base['cpf'] = df_dados_da_base['cpf'].astype(str).str.replace('-','', regex=True)
+    df_dados_da_base['cpf'] = df_dados_da_base['cpf'].astype(str).str.replace('.','', regex=True)
+    df_dados_da_base['cpf'] = df_dados_da_base['cpf'].astype(str).str.replace(' ','', regex=True)    
     
     #salvar o excel com os dados do banco para o dia buscado
-    df_dados_da_base.sort_values(by=['id_sasi']).to_excel('/home/thiago/Documentos/notebooks/planilhas_diarias/dados_cartao_dia_'+str(data_de_operacao)+'.xlsx',index=False)
-    
+    df_dados_da_base.sort_values(by=['id_sasi']).to_excel('C:/Users/DEV-GW-POWER/Documents/dados_op_enchente_2022/planilhas_diarias/dados_cartao_dia_'+str(data_de_operacao)+'.xlsx',
+    index=False, 
+    encoding= 'unicode_escape')
+
+    print('Finalizou dia :D',data_de_operacao);
     
     #próximo dia
     data_de_operacao = data_de_operacao + timedelta(days=1)
